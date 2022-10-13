60b55d94739b075c97fecd251a9fd85d42a13975
riduanSPL <riduan@saranaprimalestari.com>
2022-10-13 17:39:39 +0800

    test commit notificatiion

--- app/Http/Controllers/Admin/AdminNotificationController.php
+++ app/Http/Controllers/Admin/AdminNotificationController.php
@@ -12,7 +12,7 @@ class AdminNotificationController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     */
+     */  
     public function index()
     {
         //
